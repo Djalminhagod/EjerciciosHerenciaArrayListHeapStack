f2e30785720b840b222759949d849a0db57f65f8
Djalminhagod <r.senra@liceolapaz.net>
2026-02-05 13:12:49 +0100

    inicio ejercicio 3

---
+++ src/ejercicio3/producto.java
@@ -0,0 +1,11 @@
+package ejercicio3;
+
+public class producto {
+    protected String nombre;
+    protected double precio;
+
+    public producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
